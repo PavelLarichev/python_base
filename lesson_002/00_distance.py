87a05e573b6a2de454ee5fc1372d90ab3be103f4
Pavel Larichev <serpentinedreamweaver@gmail.com>
2020-04-24 17:05:16 +0300

    complete 00_distance

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
+from pprint import pprint
 
 sites = {
     'Moscow': (550, 370),
@@ -13,10 +14,28 @@ sites = {
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 distances = {}
+moscow = sites["Moscow"]
+london = sites["London"]
+paris = sites["Paris"]
 
-# TODO здесь заполнение словаря
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
 
-print(distances)
+distances["Moscow"] = {}
+distances["Moscow"]["London"] = moscow_london
+distances["Moscow"]["Paris"] = moscow_paris
+
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+distances["London"] = {}
+distances["London"]["Moscow"] = moscow_london
+distances["London"]["Paris"] = london_paris
+
+distances["Paris"] = {}
+distances["Paris"]["Moscow"] = moscow_paris
+distances["Paris"]["London"] = london_paris
+pprint(distances)
+
+#print(distances)
 
 
 
