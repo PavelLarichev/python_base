4dc3f0c19c03b749a5bae0b142209afa18986051
Pavel Larichev <serpentinedreamweaver@gmail.com>
2020-04-30 13:49:21 +0300

    complete 02_envelope 1st part

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -21,7 +21,12 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print("ДА")
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    print("ДА")
+else:
+    print("НЕТ")
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
