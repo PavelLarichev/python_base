04db534ca9510eb90704e9668e869127d447a8ef
Pavel Larichev <serpentinedreamweaver@gmail.com>
2020-05-12 18:27:07 +0300

    complete 06_wall

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+step = 0
+for y in range(0, 1000, 50):
+    y1 = y + 50
+    step -= 50
+    for x in range(step, 1000, 100):
+        x1 = x + 100
+        left_bottom = sd.get_point(x, y)
+        right_top = sd.get_point(x1, y1)
+        sd.rectangle(left_bottom=left_bottom, right_top=right_top, color=(127, 63, 0), width=2)
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
@@ -17,4 +25,4 @@ import simple_draw
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
 
-simple_draw.pause()
+sd.pause()
