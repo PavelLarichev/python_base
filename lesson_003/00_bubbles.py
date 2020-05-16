994c41187770644f33a44917c4725b77f471dff1
Pavel Larichev <serpentinedreamweaver@gmail.com>
2020-05-16 16:19:40 +0300

    complete 00_bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -2,22 +2,49 @@
 
 import simple_draw as sd
 
+
+import random
+
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+# point = sd.get_point(300, 300)
+# radius = 50
+# for _ in range(3):
+#     radius += 5
+#     sd.circle(point, radius=radius)
 
 # Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
-# TODO здесь ваш код
+
+
+def bubble(point, step, color, width):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(point, radius=radius, color=color, width=width)
+
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+
+
+# for x in range(100, 1001, 100):
+#     point = sd.get_point(x, 100)
+#     bubble(point=point, step=5, color=sd.random_color(), width=2)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+#
+# for y in range(100, 301, 100):
+#     for x in range(100, 1001, 100):
+#         point = sd.get_point(x, y)
+#         bubble(point=point, step=5, color=sd.random_color(), width=2)
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+
+
+for _ in range(100):
+    point = sd.random_point()
+    bubble(point=point, step=random.randint(1, 10), color=sd.random_color(), width=random.randint(1, 3))
 
 sd.pause()
 
