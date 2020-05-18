91abd43fc2f6b6ed7a30084737e50d02e93a06cf
Pavel Larichev <serpentinedreamweaver@gmail.com>
2020-05-18 13:32:32 +0300

    edit 06_rainbow

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -10,12 +10,13 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 x, y = 50, 50
 x1, y1 = 350, 450
+step = 5
 for color in rainbow_colors:
-    x += 5 # TODO помести 5-ку в переменную, т.к. ты используешь её несколько раз, и в width можно тоже её присваивать
-    x1 += 5
+    x += step
+    x1 += step
     sp = sd.get_point(x, y)
     ep = sd.get_point(x1, y1)
-    print(sd.line(start_point=sp, end_point=ep, color=color, width=4))
+    print(sd.line(start_point=sp, end_point=ep, color=color, width=step))
 # Подсказка: цикл нужно делать сразу по тьюплу с цветами радуги.
 
 
