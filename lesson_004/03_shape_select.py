f9a0c315625a499d80caa64385df00bd49515620
Павел Ларичев <serpentinedreamweaver@gmail.com>
2020-06-17 15:55:16 +0300

    complete 02_shape_select

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,61 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+sd.resolution = 800, 800
+
+
+def triangle(point, angle, length, color):
+    next_point = point
+    for angle in range(angle, angle + 240, 120):
+        v1 = sd.get_vector(start_point=next_point, angle=angle, length=length, width=2)
+        v1.draw(color=color)
+        next_point = v1.end_point
+    sd.line(start_point=next_point, end_point=point, width=2, color=color)
+
+
+def square(point, angle, length, color):
+    next_point = point
+    for angle in range(angle, angle + 270, 90):
+        v1 = sd.get_vector(start_point=next_point, angle=angle, length=length, width=2)
+        v1.draw(color=color)
+        next_point = v1.end_point
+    sd.line(start_point=next_point, end_point=point, width=2, color=color)
+
+
+def pentagon(point, angle, length, color):
+    next_point = point
+    for angle in range(angle, angle + 288, 72):
+        v1 = sd.get_vector(start_point=next_point, angle=angle, length=length, width=2)
+        v1.draw(color=color)
+        next_point = v1.end_point
+    sd.line(start_point=next_point, end_point=point, width=2, color=color)
+
+
+def hexagon(point, angle, length, color):
+    next_point = point
+    for angle in range(angle, angle + 300, 60):
+        v1 = sd.get_vector(start_point=next_point, angle=angle, length=length, width=2)
+        v1.draw(color=color)
+        next_point = v1.end_point
+    sd.line(start_point=next_point, end_point=point, width=2, color=color)
+
+
+def user_input():
+    print('Возможные фигуры:\n0 : треугольник \n1 : квадрат \n2 : ппятиугольник \n3 : шестиугольник \n')
+    number_input = input('Введите желаемую фигуру: ')
+    figure = int(number_input)
+    if figure == 0:
+        triangle(point=sd.get_point(350, 300), angle=45, length=175, color=sd.COLOR_DARK_GREEN)
+    elif figure == 1:
+        square(point=sd.get_point(400, 300), angle=62, length=150, color=sd.COLOR_DARK_GREEN)
+    elif figure == 2:
+        pentagon(point=sd.get_point(400, 300), angle=41, length=100, color=sd.COLOR_DARK_GREEN)
+    elif figure == 3:
+        hexagon(point=sd.get_point(475, 320), angle=88, length=100, color=sd.COLOR_DARK_GREEN)
+    else:
+        print('Вы ввели некорректный номер:')
+
+
+user_input()
 
 sd.pause()
