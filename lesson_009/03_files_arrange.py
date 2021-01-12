80f3ab928ceaae97ca0163d3589065ef0df5e1d4
Павел Ларичев <serpentinedreamweaver@gmail.com>
2021-01-12 14:42:04 +0300

    09/03 done

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -40,7 +40,54 @@ import shutil
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+class FileSorter:
+
+    def __init__(self, src, dst):
+        if src:
+            self.src = os.path.normpath(src)
+        else:
+            print('Не указан каталог источник.')
+        if dst:
+            self.dst = os.path.normpath(dst)
+        else:
+            print('Не указан каталог назначения.')
+
+    def get_dirs(self, mtime=None):
+        file_mdate = time.gmtime(mtime)
+        year, mon = str(file_mdate.tm_year), str(file_mdate.tm_mon)
+        file_dest = os.path.join(self.dst, *(year, mon if len(mon) != 1 else '0' + mon))
+        return file_dest
+
+    def make_dirs(self, dir=None):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    def copy_file(self, src=None, dst=None):
+        if not os.path.exists(dst):
+            shutil.copy2(src=src, dst=dst)
+
+    def arrange(self):
+        for dir_path, dir_names, file_names in os.walk(self.src):
+            for file in file_names:
+                file_mtime = os.path.getmtime(os.path.join(dir_path, file))
+                file_dst = self.get_dirs(file_mtime)
+                self.make_dirs(file_dst)
+
+                file_dst = os.path.join(file_dst, file)
+                file_src = os.path.join(dir_path, file)
+                self.copy_file(src=file_src, dst=file_dst)
+
+        if os.path.exists(self.dst):
+            print(f'Сортировка завершена успешно.', {self.dst})
+        else:
+            print(f'Проверьте пути к каталогам.', {self.src}, {self.dst})
+
+
+if __name__ == '__main__':
+    src_dir = 'C:\\Users\\iamalreadydead\\Pictures\\flat'
+    dst_dir = 'C:\\Users\\iamalreadydead\\Pictures\\flat\\sorted'
+    files = FileSorter(src=src_dir, dst=dst_dir)
+    files.arrange()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
